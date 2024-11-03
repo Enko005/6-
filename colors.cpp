95167cc1123c63cd3c05b41c7ee7e30032be73e3
Enko005 <mxmgalai@gmail.com>
2024-11-03 16:52:02 +0200

    змінено вибір кольору на фрукт

--- colors.cpp
+++ colors.cpp
@@ -6,21 +6,22 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int x;
-	cout << "Введіть номер кольору x: ";
+	cout << "Введіть номер фрукту x: ";
 	cin >> x;
 	switch (x)
 	{
-	case 1: cout << "Синій"; break;
-	case 2: cout << "Червоний"; break;
-	case 3: cout << "Зелений"; break;
-	case 4: cout << "Чорний"; break;
-	case 5: cout << "Жовтий"; break;
-	case 6: cout << "Помаранчевий"; break;
-	case 7: cout << "Білий"; break;
-	case 8: cout << "Рожевий"; break;
-	case 9: cout << "Коричневий"; break;
-	case 10: cout << "Фіолетовий"; break;
+	case 1: cout << "гранат"; break;
+	case 2: cout << "кумкват"; break;
+	case 3: cout << "яблоко"; break;
+	case 4: cout << "лимон"; break;
+	case 5: cout << "слива"; break;
+	case 6: cout << "абрикос"; break;
+	case 7: cout << "авокадо"; break;
+	case 8: cout << "гуава"; break;
+	case 9: cout << "бергамот"; break;
+	case 10: cout << "ківи"; break;
 	default: cout << "Не існує"; break;
 	}
 }
 
+ц
