9323ba70152c9d99598dcdc5fd0b1589e3345cc3
Enko005 <mxmgalai@gmail.com>
2024-11-04 11:16:25 +0200

    Зміна вибору фрукта на відеогру

--- colors.cpp
+++ colors.cpp
@@ -6,22 +6,20 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int x;
-	cout << "Введіть номер фрукту x: ";
+	cout << "Введіть номер відеогри x: ";
 	cin >> x;
 	switch (x)
 	{
-	case 1: cout << "гранат"; break;
-	case 2: cout << "кумкват"; break;
-	case 3: cout << "яблоко"; break;
-	case 4: cout << "лимон"; break;
-	case 5: cout << "слива"; break;
-	case 6: cout << "абрикос"; break;
-	case 7: cout << "авокадо"; break;
-	case 8: cout << "гуава"; break;
-	case 9: cout << "бергамот"; break;
-	case 10: cout << "ківі"; break;
+	case 1: cout << "Cyberpunk 2077"; break;
+	case 2: cout << "League of Legends"; break;
+	case 3: cout << "Valorant"; break;
+	case 4: cout << "Minecraft"; break;
+	case 5: cout << "Call of Duty: Warzone"; break;
+	case 6: cout << "Among Us"; break;
+	case 7: cout << "Dota 2"; break;
+	case 8: cout << "Brawl Stars"; break;
+	case 9: cout << "Undertale"; break;
+	case 10: cout << "Tetris (Nintendo)"; break;
 	default: cout << "Не існує"; break;
 	}
 }
-
-ц
